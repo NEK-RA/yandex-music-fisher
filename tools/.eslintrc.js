272388a3a767153ac39862cc055061935b44716e
Artyom Egorov <mail@egoroof.ru>
2016-11-26 18:13:59 +0800

    обновлены зависимости

--- tools/.eslintrc.js
+++ tools/.eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
     "env": {
         "es6": true,
         "browser": true,
@@ -14,16 +17,16 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "rules": { // http://eslint.org/docs/rules/
-        "indent": 0,
-        "linebreak-style": 0,
-        "quotes": [1, "single"],
-        "semi": [2, "always"],
-        "no-console": 0,
-        "no-var": 2, // require let or const instead of var
-        "prefer-arrow-callback": 2, // suggest using arrow functions as callbacks
-        "prefer-const": 2, // suggest using const declaration for variables that are never modified after declared
-        "prefer-rest-params": 2, // suggest using the rest parameters instead of arguments
-        "prefer-spread": 2, // suggest using the spread operator instead of .apply().
-        "prefer-template": 2 // suggest using template literals instead of strings concatenation
+        "indent": "off",
+        "linebreak-style": "off",
+        "quotes": ["warn", "single"],
+        "semi": ["error", "always"],
+        "no-console": "off",
+        "no-var": "error", // require let or const instead of var
+        "prefer-arrow-callback": "error", // suggest using arrow functions as callbacks
+        "prefer-const": "error", // suggest using const declaration for variables that are never modified after declared
+        "prefer-rest-params": "error", // suggest using the rest parameters instead of arguments
+        "prefer-spread": "error", // suggest using the spread operator instead of .apply().
+        "prefer-template": "error" // suggest using template literals instead of strings concatenation
     }
 };
